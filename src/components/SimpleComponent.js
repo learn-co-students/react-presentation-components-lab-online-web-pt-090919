43c0561c7039b2b574aa462bd94e7af1c7c66dca
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-31 06:23:58 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,19 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react';
+
+export default class SimpleComponent extends Component {
+  state = {
+    mood: 'happy'
+  }
+  
+  handleClick = () => {
+    this.setState({mood: 'sad'})
+  }
+  render(){
+    return(
+      <div>{this.state.mood}
+      <button onClick={this.handleClick}>Click to change mood</button>
+      </div>
+      )
+  }
+}
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,3 @@
 // Code SimplerComponent Here
+import React, {PureComponent} from 'react'
+const SimplerComponent = (props) => <div>I am just {props.mood}</div>
