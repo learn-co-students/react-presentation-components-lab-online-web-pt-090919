15505a60a40f72ecd6489fe42f10ac7fe6a614f4
Edward Heaver <heaver.edward02@gmail.com>
2020-06-17 09:54:42 +0100

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,8 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+export default function SimplerComponent({handleClick}) {
+    return (
+        <div onClick={handleClick}>I am just happy.</div>
+    )
+}
